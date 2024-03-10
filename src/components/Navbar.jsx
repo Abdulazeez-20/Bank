1da4af11be15c963616d61550c0c427b63c74bf3
Aziz <Abdulazeez_20@yahoo.com>
2024-03-10 22:21:30 +0300

    coding the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
     return (
-        <div className='text-pink-600 t'>where are you</div>
+        <nav className='flex items-center justify-center w-full py-6'>
+            <img src={logo} alt="hoobank" className='w-[125px] h-[32px]' />
+            <ul className='items-center justify-end flex-1 list-none sm:flex'>
+                {navLinks.map((nav, index) => (
+                    <li
+                        className={`font-normal font-poppins cursor-pointer
+                 text-white ${index === navLinks.length - 1 ? mr - 0 : mr - 10}`}
+                    >
+                        <a href={`#${nav.id}`}>{`${nav.title}`}</a>
+                    </li>
+
+                ))}
+            </ul>
+        </nav>
     )
 }
 
