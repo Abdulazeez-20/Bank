0b7065d7dcd8ddea5881e86ec606d0c827ac4f61
Aziz <Abdulazeez_20@yahoo.com>
2024-03-12 23:20:35 +0300

    started with the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from '../styles'
+import { discount, robot } from '../assets'
 
-const Hero = () => {
-    return (
-        <div>Hero</div>
-    )
-}
+const Hero = () => (
+    <section id="home" className={`flex flex-col md:flex-row ${styles.paddingY}`} >
+
+    </section>
+)
 
 export default Hero
