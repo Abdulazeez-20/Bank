3002907e05395dee3a4860010282918052583fb0
Aziz <Abdulazeez_20@yahoo.com>
2024-03-13 23:42:56 +0300

    working on Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,26 @@ import { discount, robot } from '../assets'
 
 const Hero = () => (
     <section id="home" className={`flex flex-col md:flex-row ${styles.paddingY}`} >
+        <div className={`flex-1 ${styles.flexStart} px-6 xl:px-0 sm:px-16  flex-col`}>
+            <div className='flex flex-row items-center py-2 px-4 
+            bg-discount-gradient rounded-[10px] mb-2'>
+                <img src={discount} alt="discound" className='w-[32px] h-[32px]' />
+                <p className={`${styles.paragraph}`}>
+                    <span className='text-white'>20%</span> Discount for {" "}
+                    <span className='text-white'>1 Month</span> Account
+                </p>
+            </div>
 
+            <div className='flex flex-row items-center justify-between w-full'>
+                <h1 className='text-[52px] text-white
+                flex-1 font-poppins font-semibold ss:text-[72px]
+                ss:leading-[100px] leading-[75px]'>
+                    The Next <br className='hidden sm:block' />
+                    <span className='text-gradient'>Generation</span> {" "}
+                    Payment Method.
+                </h1>
+            </div>
+        </div>
     </section>
 )
 
